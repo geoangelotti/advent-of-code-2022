17ba2f761b4b64e369a90f52c2368793e31fa616
Geo Angelopoulos <geo.angelotti@gmail.com>
2023-03-17 11:01:45 +0100

    skeleton

---
+++ rust/day_11/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn process_part_1(input: &str) -> String {
+    "".to_string()
+}
+
+pub fn process_part_2(input: &str) -> String {
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_works() {
+        let result = process_part_1("");
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn part_2_works() {
+        let result = process_part_2("");
+        assert_eq!(result, "");
+    }
+}
