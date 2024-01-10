cc96ffc8d5870fb5291d66c636070bb1af61e0a3
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-01-10 15:55:08 +0100

    part 2 solution

--- rust/day_12/src/lib.rs
+++ rust/day_12/src/lib.rs
@@ -87,6 +87,74 @@ pub fn process_part_1(input: &str) -> String {
     res[&(end.0, end.1, 'z')].to_string()
 }
 
+pub fn process_part_2(input: &str) -> String {
+    let (_, grid) = grid(input).unwrap();
+    let end = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(i, v)| v.iter().enumerate().zip(std::iter::repeat(i)))
+        .find_map(|((x, &c), y)| {
+            if c == 'E' {
+                Some((x as i32, y as i32))
+            } else {
+                None
+            }
+        })
+        .unwrap();
+    let grid: Vec<Vec<char>> = grid
+        .iter()
+        .map(|vec| {
+            vec.iter()
+                .map(|c| match c {
+                    'S' => 'a',
+                    'E' => 'z',
+                    v => *v,
+                })
+                .collect()
+        })
+        .collect();
+    let edges = (0i32..(grid.len() as i32))
+        .cartesian_product(0i32..(grid[0].len() as i32))
+        .flat_map(|(y, x)| {
+            let neighbours = vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
+            let current_node_id = (x, y);
+            neighbours
+                .iter()
+                .filter_map(|cell| {
+                    grid.get(cell.1 as usize)
+                        .and_then(|vec| vec.get(cell.0 as usize))
+                        .and_then(|existing_cell| {
+                            let current_node_height = grid[y as usize][x as usize];
+                            if current_node_height as u8 + 1 >= *existing_cell as u8 {
+                                Some((
+                                    (current_node_id.0, current_node_id.1, current_node_height),
+                                    (cell.0, cell.1, *existing_cell),
+                                ))
+                            } else {
+                                None
+                            }
+                        })
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<((i32, i32, char), (i32, i32, char))>>();
+    let graph = DiGraphMap::<(i32, i32, char), ()>::from_edges(edges.iter().map(|(a, b)| (*b, *a)));
+    let res = dijkstra(&graph, (end.0, end.1, 'z'), None, |_| 1);
+    res.iter()
+        .filter_map(
+            |(node, cost)| {
+                if node.2 == 'a' {
+                    Some(*cost)
+                } else {
+                    None
+                }
+            },
+        )
+        .min()
+        .unwrap()
+        .to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -101,5 +169,11 @@ abdefghi";
     fn part_1_works() {
         let result = process_part_1(INPUT);
         assert_eq!(result, "31");
+    }
+
+    #[test]
+    fn part_2_works() {
+        let result = process_part_2(INPUT);
+        assert_eq!(result, "29");
     }
 }
